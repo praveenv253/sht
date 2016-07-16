fbc0268245b87c2ff90f455856a4f083a5de0347
Praveen Venkatesh <praveenv253@gmail.com>
2016-07-15 22:51:32 -0400

    Edit plot_grid to plot functions on the sphere.

--- plot_grid.m
+++ plot_grid.m
@@ -1,29 +1,37 @@
-L = 20;
-load(sprintf('grid_L%d.mat', L));
+function plot_grid(filename)
+	L = 4;
+	load(sprintf('grid_L%d.mat', L));
 
-x = sin(thetas) .* cos(phis);
-y = sin(thetas) .* sin(phis);
-z = cos(thetas);
+	x = sin(thetas) .* cos(phis);
+	y = sin(thetas) .* sin(phis);
+	z = cos(thetas);
 
-scatter3(x, y, z, 20, 'b', 'filled');
-hold on;
+	if nargin == 0
+		scatter3(x, y, z, 20, 'b', 'filled');
+	else
+		load(filename);   % This file should define 'f'
+		scatter3(x, y, z, 20, imag(f), 'filled');
+	end
 
-% Underlying sphere
-vw = [70 25];
-res = 201;
-phi = linspace(0, 2 * pi, res);
-theta = linspace(0, pi, ceil(res/2));
-[Phi, Theta] = meshgrid(phi, theta);
+	%hold on;
 
-r = 0.98;
-X = r .* sin(Theta) .* cos(Phi);
-Y = r .* sin(Theta) .* sin(Phi);
-Z = r .* cos(Theta);
+	% Underlying sphere
+	%vw = [70 25];
+	%res = 201;
+	%phi = linspace(0, 2 * pi, res);
+	%theta = linspace(0, pi, ceil(res/2));
+	%[Phi, Theta] = meshgrid(phi, theta);
 
-surf(X, Y, Z, 0.8 * ones(size(X)));
-colormap(gray);
-caxis([0, 1]);
-shading interp;
-daspect([1 1 1]);
-axis tight;
-view(vw);
+	%r = 0.98;
+	%X = r .* sin(Theta) .* cos(Phi);
+	%Y = r .* sin(Theta) .* sin(Phi);
+	%Z = r .* cos(Theta);
+
+	%surf(X, Y, Z, 'g');
+	%colormap(gray);
+	%caxis([0, 1]);
+	%shading interp;
+	%daspect([1 1 1]);
+	%axis tight;
+	%view(vw);
+end
