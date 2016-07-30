addf8d69980164d543fcbbfaf8e05158b40312be
Praveen Venkatesh <praveenv253@gmail.com>
2016-07-30 01:14:13 -0400

    Merge test_argsort into test_utils.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,10 @@
 from __future__ import print_function, division
 
 import numpy as np
-from sht.utils import l_to_lm
+
+from sht import isht
+from sht.grids import standard_grid
+from sht.utils import l_to_lm, argsort_thetaphi_wrt_theta
 
 def test_l_to_lm():
     L = 10
@@ -20,3 +23,16 @@ def test_l_to_lm():
     assert np.array_equal(z[l**2 + l], x)
     z = np.delete(z, l**2 + l)
     assert np.array_equal(z, np.zeros(L**2 - L))
+
+
+def test_argsort():
+    L = 10
+    thetas, phis = standard_grid(L)
+
+    x = np.zeros(L)
+    x[1] = 1
+    x = l_to_lm(x)
+    y = isht(x, thetas, phis).real
+
+    i = argsort_thetaphi_wrt_theta(thetas)
+    assert np.all(np.abs(np.sort(y)[::-1] - y[i]) < 1e-15)
