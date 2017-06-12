a033df7df624ba0e7535a4bce46b8455c41e1e29
Praveen Venkatesh <praveenv253@gmail.com>
2017-06-12 01:29:45 -0400

    Note issue in __init__.py and make temp fix.

--- sht/__init__.py
+++ sht/__init__.py
@@ -7,7 +7,13 @@ from . import grids
 # Try importing the plot module. This will fail if mayavi isn't installed.
 # But requiring mayavi is too much overhead for the other sht modules. The plot
 # module will work automatically if mayavi is installed.
-try:
-    from . import plot
-except ImportError:
-    pass
+
+# FIXME This creates a problem with scripts that need to import sht, but
+# which are run on a machine that don't have access to an X-server. These
+# scripts crash with "cannot connect to X server". We need to give scripts the
+# option to not import the plot module. Until we figure out how to do that,
+# this will be commented out.
+#try:
+#    from . import plot
+#except ImportError:
+#    pass
